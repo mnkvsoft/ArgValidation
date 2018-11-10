2c70f2968343fac1c849f2d498a0065962c3d647
mynkov <mynkov@yandex.ru>
2018-11-10 10:01:33 +0300

    Refactoring ArgumentStringExtension

---
+++ ArgValidation/Internal/ConditionCheckers/ObjectConditionChecker.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ArgValidation.Internal.ExceptionThrowers;
+
+namespace ArgValidation.Internal
+{
+    internal static class ConditionChecker
+    {
+        public static bool IsDefault<T>(T obj)
+        {
+            return EqualityComparer<T>.Default.Equals(obj, default(T));
+        }
+
+        public static bool IsEqual<T>(T obj1, T obj2)
+        {
+            return EqualityComparer<T>.Default.Equals(obj1, obj2);
+        }
+        
+        internal static bool OnlyValues<T>(Argument<T> arg, T[] values)
+        {
+            InvalidMethodArgumentThrower.IfArgumentIsNullForOnlyValues(values, nameof(values));
+            return values.Any(x => IsEqual(arg.Value, x));
+        }
+
+        public static bool MoreThan<T>(Argument<T> arg, T moreThan) where T : IComparable<T>
+        {
+            InvalidMethodArgumentThrower.IfNullForComparable(moreThan, nameof(moreThan));
+            InvalidMethodArgumentThrower.IfNullForComparable(arg.Value, arg.Name);
+
+            return arg.Value.CompareWith(moreThan) > 0;
+        }
+
+        public static bool LessThan<T>(Argument<T> arg, T lessThan) where T : IComparable<T>
+        {
+            InvalidMethodArgumentThrower.IfNullForComparable(lessThan, nameof(lessThan));
+            InvalidMethodArgumentThrower.IfNullForComparable(arg.Value, arg.Name);
+
+            return arg.Value.CompareWith(lessThan) < 0;
+        }
+        
+        public static bool Max<T>(Argument<T> arg, T max) where T : IComparable<T>
+        {
+            InvalidMethodArgumentThrower.IfNullForComparable(max, nameof(max));
+            InvalidMethodArgumentThrower.IfNullForComparable(arg.Value, arg.Name);
+
+            return arg.Value.CompareWith(max) <= 0;
+        }
+        
+        public static bool Min<T>(Argument<T> arg, T min) where T : IComparable<T>
+        {
+            InvalidMethodArgumentThrower.IfNullForComparable(min, nameof(min));
+            InvalidMethodArgumentThrower.IfNullForComparable(arg.Value, arg.Name);
+
+            return arg.Value.CompareWith(min) >= 0;
+        }
+
+        internal static bool InRange<T>(Argument<T> arg, T min, T max) where T : IComparable<T>
+        {
+            if (IsEqual(arg.Value, min) && IsEqual(arg.Value, max))
+                return true;
+
+            InvalidMethodArgumentThrower.IfArgumentIsNullForRange(max, nameof(max));
+            InvalidMethodArgumentThrower.IfArgumentIsNullForRange(min, nameof(min));
+            InvalidMethodArgumentThrower.IfNullForRange(arg, min, max);
+            InvalidMethodArgumentThrower.IfNotRange(min, max);
+
+            return arg.Value.InRange(min, max);
+        }
+        
+        public static bool LessOrEqualThan<T>(Argument<T> arg, T lessOrEqualThan) where T : IComparable<T>
+        {
+            if (IsEqual(arg.Value, lessOrEqualThan))
+                return true;
+
+            if (LessThan(arg, lessOrEqualThan))
+                return true;
+
+            return false;
+        }
+        
+        public static bool MoreOrEqualThan<T>(Argument<T> arg, T moreOrEqualThan) where T : IComparable<T>
+        {
+            if (IsEqual(arg.Value, moreOrEqualThan))
+                return true;
+
+            if (MoreThan(arg, moreOrEqualThan))
+                return true;
+
+            return false;
+        }
+    }
+}
