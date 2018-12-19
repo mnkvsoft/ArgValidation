bb62301707d1bc15471e09c448e1269d1084c749
Viktor Minkow <vminkow@plazius.ru>
2018-12-19 09:12:53 +0300

    Edit Example

--- ArgValidation/AssemblyInfo.cs
+++ ArgValidation/AssemblyInfo.cs
@@ -1,3 +1,9 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("ArgValidation.Tests")]
+//[assembly: AssemblyInformationalVersion("1.2.0")]
+//[assembly: AssemblyCompany("Viktor Mynkov")]
+//[assembly: AssemblyTitle("ArgValidation")]
+//[assembly: AssemblyDescription("A simply arguments validation library with fluent API")]
+//[assembly: AssemblyCopyright("Viktor Mynkov")]
